4a7a1d705afea23d9bb7d5a3fcef67788e64a262
andygg <andreas.gebert.ch@gmail.com>
2019-10-19 01:22:07 +0200

    Created basic barebone of the GAM application.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './template/header/header.component';
+import { TitleComponent } from './template/title/title.component';
+import { FilterComponent } from './template/filter/filter.component';
+import { SubtitleComponent } from './template/subtitle/subtitle.component';
+import { ItemDescriptionComponent } from './template/item-description/item-description.component';
+import { ItemListComponent } from './template/item-list/item-list.component';
+import { ListItemComponent } from './template/list-item/list-item.component';
+import { FooterComponent } from './template/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TitleComponent,
+    FilterComponent,
+    SubtitleComponent,
+    ItemDescriptionComponent,
+    ItemListComponent,
+    ListItemComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/template/subtitle/subtitle.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-subtitle',
+  templateUrl: './subtitle.component.html',
+  styleUrls: ['./subtitle.component.sass']
+})
+export class SubtitleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
